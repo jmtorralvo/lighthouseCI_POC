87e3d1abe95a264c0a9a0d5acce3c51bf121e779
jmtorralvo <jm.torralvo@one-beyond.com>
2025-03-05 14:52:23 +0100

    feat(): include assertions

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -4,6 +4,12 @@ module.exports = {
 			startServerCommand: "npm run dev",
 			url: ["http://localhost:3000"],
 		},
+		assert: {
+			assertions: {
+				"categories:performance": ["warn", { minScore: 0.9 }],
+				"categories:accessibility": ["error", { minScore: 1 }],
+			},
+		},
 		upload: {
 			target: "temporary-public-storage",
 		},
